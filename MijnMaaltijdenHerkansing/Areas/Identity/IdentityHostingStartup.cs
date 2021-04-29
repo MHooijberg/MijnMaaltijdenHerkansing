ac3be614b84c35b70170b250cca45ac746b8b95c
Psychopath24 <m.hooijberg24@gmail.com>
2021-04-29 10:33:35 +0100

    Inlog systeem scaffolding

---
+++ MijnMaaltijdenHerkansing/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MijnMaaltijdenHerkansing.Areas.Identity.Data;
+using MijnMaaltijdenHerkansing.Data;
+
+[assembly: HostingStartup(typeof(MijnMaaltijdenHerkansing.Areas.Identity.IdentityHostingStartup))]
+namespace MijnMaaltijdenHerkansing.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MijnMaaltijdenHerkansingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MijnMaaltijdenHerkansingContextConnection")));
+
+                services.AddDefaultIdentity<Gebruiker>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MijnMaaltijdenHerkansingContext>();
+            });
+        }
+    }
+}
