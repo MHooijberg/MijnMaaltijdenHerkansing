fcfdc2dd13dde9dbd8e5a4a76f1e7c0af5dd9a06
nielsnl4 <nielsrenkema@gmail.com>
2021-06-08 22:50:20 +0200

    Routing

---
+++ MijnMaaltijdenHerkansing/Areas/Maaltijd/Pages/Index.cshtml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MijnMaaltijdenHerkansing.Data;
+using MijnMaaltijdenHerkansing.Models;
+
+namespace MijnMaaltijdenHerkansing.Pages
+{
+    public class MaaltijdModel : PageModel
+    {
+
+        private readonly MijnMaaltijdenHerkansingContext _context;
+
+        [BindProperty]
+        public int? _id { get; set; }
+
+        [BindProperty]
+        public Post post { get; set; }
+
+        public Maaltijd maaltijd { get; set; }
+
+        public MaaltijdModel(MijnMaaltijdenHerkansingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            _id = id;
+
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            post = _context.Posts.Find(id);
+
+            if(post == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                maaltijd = _context.Maaltijden.Find(post.MaaltijdId);
+            }
+
+            return Page();
+        }
+    }
+}
